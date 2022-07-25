a5b3aeb9eaf9ba2607243e5ba22ed3611c31b53d
Jonas Martins <jonas_silva_rio@hotmail.com>
2022-07-25 12:46:28 -0300

    Adicionando desafio e outras correções

---
+++ desafiolojainterativa/scriptlji.js
@@ -0,0 +1,53 @@
+var formatter = new Intl.NumberFormat('pt-BR', {
+style: 'currency',
+currency: 'BRL',
+
+// These options are needed to round to whole numbers if that's what you want.
+//minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+//maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
+
+
+function AtualizaTable(listaProduto){
+    const tableElement = document.getElementById("table");
+    var tabela = "";
+    
+    for(let i=0; i< listaProduto.length; i++){
+        let produto = listaProduto[i];
+        let tr = `<tr>
+                    <td>${produto.nomeProduto}</td>
+                    <td>${produto.fabProduto}</td>
+                    <td>${produto.categProduto}</td>
+                    <td>${produto.quantProduto}</td>
+                    <td>R$ ${produto.valorProduto}</td>
+                    <td><button id="deleteBtn" onclick="remover(${i})">Delete</button></td>
+                </tr>`;
+        tabela += tr;
+    }
+    tableElement.innerHTML = tabela;
+}
+
+function remover(indice){
+    listaProduto.splice(indice,1);
+    AtualizaTable(listaProduto);
+}
+
+function Adiciona(){
+    var nomeProduto = document.getElementById("nomeProduto").value;
+    var fabProduto = document.getElementById("fabProduto").value;
+    var categProduto = document.getElementById("categProduto").value;
+    var quantProduto = document.getElementById("quantProduto").value;
+    var valorProduto = parseFloat(document.getElementById("valorProduto").value);
+    listaProduto.push({
+        nomeProduto,
+        fabProduto,
+        categProduto,
+        quantProduto,
+        valorProduto
+    })
+    AtualizaTable(listaProduto);
+}
+const listaProduto = [{nomeProduto:"Leite",fabProduto:"Nestle",categProduto:"Leite",quantProduto:2,valorProduto:8}];
+function Load(){
+    AtualizaTable(listaProduto);
+};
