74b7a4ef8d6672134f6f76be0a7d13329a29b099
Jonas Martins <37028535+jonsmartins@users.noreply.github.com>
2024-10-28 18:54:49 -0300

    add crud fullstack

--- portfolio-v2/src/components/ProjectsContainer.jsx
+++ portfolio-v2/src/components/ProjectsContainer.jsx
@@ -7,7 +7,7 @@ const ProjectsContainer = () => {
       <p>Aqui alguns de meus projetos:</p>
       <div className="links">
       <a href="https://github.com/jonsmartins/portifolio" className="btn" target="_blank">Portfolio</a>
-      <a href="https://github.com/jonsmartins/projetoFullstack" className="btn" target="_blank">Projeto fullstack</a>
+      <a href="https://github.com/jonsmartins/crud-react-node" className="btn" target="_blank">Projeto fullstack</a>
       <a href="https://github.com/jonsmartins/github_api" className="btn" target="_blank">Consumo de API</a>
       </div>
     </section>
